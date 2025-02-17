3830a1a994fa2618b6cca09d204066175c8d2c82
stevenmcsorley <smcsorl@sky.com>
2025-02-17 23:09:49 +0000

    Update metrics.ts

--- site3/app/routes/metrics.ts
+++ site3/app/routes/metrics.ts
@@ -1,1 +1,19 @@
+// app/routes/metrics.ts
+import { Register, collectDefaultMetrics } from 'prom-client';
+import type { LoaderFunction } from '@remix-run/node';
 
+// Create a registry
+const register = new Register();
+
+// Collect default metrics
+collectDefaultMetrics({ register });
+
+export const loader: LoaderFunction = async () => {
+  const metrics = await register.metrics();
+  return new Response(metrics, {
+    status: 200,
+    headers: {
+      'Content-Type': register.contentType,
+    },
+  });
+};
