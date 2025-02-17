d3d4ee89f82eedf8516d43215e75d5d643cbf7ae
Steven McSorley <smcsorl@sky.com>
2025-02-17 23:22:27 +0000

    prom

--- site3/app/routes/metrics.ts
+++ site3/app/routes/metrics.ts
@@ -1,19 +1,17 @@
 // app/routes/metrics.ts
-import { Register, collectDefaultMetrics } from 'prom-client';
-import type { LoaderFunction } from '@remix-run/node';
+import { collectDefaultMetrics, register } from "prom-client";
 
-// Create a registry
-const register = new Register();
+import type { LoaderFunction } from "@remix-run/node";
 
-// Collect default metrics
-collectDefaultMetrics({ register });
+// Collect default metrics into the default registry
+collectDefaultMetrics();
 
 export const loader: LoaderFunction = async () => {
   const metrics = await register.metrics();
   return new Response(metrics, {
     status: 200,
     headers: {
-      'Content-Type': register.contentType,
+      "Content-Type": register.contentType,
     },
   });
 };
