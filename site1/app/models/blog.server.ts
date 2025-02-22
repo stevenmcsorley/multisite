625068c6ce2ac35b97eb31eb572d96f3cd019d5b
Steven McSorley <smcsorl@sky.com>
2025-02-22 15:14:13 +0000

    fix harv 13

--- site1/app/models/blog.server.ts
+++ site1/app/models/blog.server.ts
@@ -1,33 +1,38 @@
 import { client } from "../utils/db.server";
 
 export type BlogPost = {
-    id: number;
-    title: string;
-    slug: string;
-    content: Record<string, string>;
-    meta_title?: string;
-    meta_description?: string;
-    excerpt?: string;
-    thumbnail_url?: string;
-    category?: string;
-    tags?: string;
-    published_at: string;
-    created_at: string;
-    updated_at: string;
-    author?: string;
-  };
-  
-export async function getAllBlogPosts(page: number, limit: number): Promise<{ rows: BlogPost[], total: number }> {
+  id: number;
+  title: string;
+  slug: string;
+  content: Record<string, string>;
+  meta_title?: string;
+  meta_description?: string;
+  excerpt?: string;
+  thumbnail_url?: string;
+  category?: string;
+  tags?: string;
+  published_at: string;
+  created_at: string;
+  updated_at: string;
+  author?: string;
+};
+
+export async function getAllBlogPosts(
+  page: number,
+  limit: number
+): Promise<{ rows: BlogPost[]; total: number }> {
   const offset = (page - 1) * limit;
   const data = await client.query(
-    `SELECT * FROM blog_posts ORDER BY published_at DESC LIMIT $1 OFFSET $2`,
+    `SELECT * FROM blog_posts ORDER BY id DESC LIMIT $1 OFFSET $2`,
     [limit, offset]
   );
   const countResult = await client.query(`SELECT COUNT(*) FROM blog_posts`);
   return { rows: data.rows, total: parseInt(countResult.rows[0].count, 10) };
 }
 
-export async function getBlogPostBySlug(slug: string): Promise<BlogPost | null> {
+export async function getBlogPostBySlug(
+  slug: string
+): Promise<BlogPost | null> {
   const result = await client.query(
     `SELECT * FROM blog_posts WHERE slug = $1`,
     [slug]
