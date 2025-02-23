c73ef9d007d6d2e77707989f78e6203c15e5a374
Steven McSorley <smcsorl@sky.com>
2025-02-23 20:11:16 +0000

    home up 2

--- site1/app/routes/_index.tsx
+++ site1/app/routes/_index.tsx
@@ -76,20 +76,20 @@ export default function IndexPage() {
         </div>
 
         {/* World Map of Name Origins */}
-        <section className="w-full p-6">
+        {/* <section className="w-full p-6">
           <OriginsMap />
-        </section>
+        </section> */}
 
         {/* Latest Blog Posts Section */}
         <section className="container mx-auto p-6">
-          <h2 className="text-3xl font-bold mb-4 text-center">
+          <h2 className="text-3xl font-bold mb-4 text-center text-white">
             Latest Blog Posts
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
             {posts.map((post) => (
               <article
                 key={post.id}
-                className="rounded-lg overflow-hidden shadow-lg"
+                className="rounded-lg overflow-hidden shadow-lg bg-white"
               >
                 <img
                   src={
