2f6249c9e18996688eb945eec7b4ab11445152e7
Steven McSorley <smcsorl@sky.com>
2025-02-25 08:17:37 +0000

    nav mob 10

--- site3/app/components/Hero.tsx
+++ site3/app/components/Hero.tsx
@@ -25,17 +25,19 @@ export function Hero({ id, ...props }: HeroProps) {
       {/* Content */}
       <div className="relative z-10 text-center text-white px-4">
         <h1 className="text-4xl md:text-6xl font-bold mb-4 uppercase">
-        Innovative Web Design & <br />
-        Development in Scotland – <br />
-        15 Years of Transforming Businesses Online
+          Innovative Web Design & <br />
+          Development in Scotland <br />
         </h1>
         <p className="text-lg md:text-xl mb-6">
-        At Half a Giraf, we craft modern, user-friendly websites that help businesses stand out, grow, and succeed.
+          At Half a Giraf, we craft modern, user-friendly websites that help
+          businesses stand out, grow, and succeed.
         </p>
         {/* “Play” Button or CTA */}
         <button
           className="flex items-center mx-auto bg-white text-black py-2 px-4 rounded-full hover:bg-gray-200 transition"
-          onClick={() => alert("Get a Free Consultation or Let’s Build Your Website")}
+          onClick={() =>
+            alert("Get a Free Consultation or Let’s Build Your Website")
+          }
         >
           <svg className="h-6 w-6 mr-2" fill="currentColor" viewBox="0 0 20 20">
             <path d="M4 4l12 6-12 6z" />
